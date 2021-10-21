3059255bb7f9d8e182b5994b9b32e7d8d8775358
maxamx <maxamx@gmail.com>
2021-10-20 23:26:58 -0300

    configurado Jakarta Persistence JPA

--- src/main/java/com/maxamx/logista/api/controller/ClienteController.java
+++ src/main/java/com/maxamx/logista/api/controller/ClienteController.java
@@ -4,6 +4,8 @@ import com.maxamx.logista.domain.model.Cliente;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -11,20 +13,13 @@ import java.util.List;
 @RestController
 public class ClienteController {
 
+    @PersistenceContext
+    private EntityManager manager;
+
     @GetMapping("/clientes")
     public List<Cliente> listar(){
-        var cliente = new Cliente();
-        cliente.setId(1L);
-        cliente.setNome("Joaõ");
-        cliente.setEmail("joao.logista@teste.com");
-        cliente.setTelefone("61 88887777");
 
-        var cliente2 = new Cliente();
-        cliente2.setId(2L);
-        cliente2.setNome("Maria");
-        cliente2.setEmail("maria.logista@teste.com");
-        cliente2.setTelefone("61 66668888");
-
-        return Arrays.asList(cliente, cliente2);
+        return manager.createQuery("from Cliente", Cliente.class)
+                .getResultList();
     }
 }
--- src/main/java/com/maxamx/logista/domain/model/Cliente.java
+++ src/main/java/com/maxamx/logista/domain/model/Cliente.java
@@ -1,15 +1,29 @@
 package com.maxamx.logista.domain.model;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 @Setter
 @Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name="cliente")
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
     private String nome;
+
     private String email;
+
+    @Column(name="telefone")
     private String telefone;
+
+
 
 }
