ee9b0e7d0be277657ad8976417e2bbd50d7a29d7
maxamx <maxamx@gmail.com>
2021-10-20 23:57:26 -0300

    repository JPA e controller RequestMapping PathVariable, streams

--- src/main/java/com/maxamx/logista/api/controller/ClienteController.java
+++ src/main/java/com/maxamx/logista/api/controller/ClienteController.java
@@ -1,7 +1,11 @@
 package com.maxamx.logista.api.controller;
 
 import com.maxamx.logista.domain.model.Cliente;
+import com.maxamx.logista.domain.model.repository.ClienteRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.EntityManager;
@@ -9,17 +13,33 @@ import javax.persistence.PersistenceContext;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+
 
 @RestController
+@AllArgsConstructor
 public class ClienteController {
 
-    @PersistenceContext
-    private EntityManager manager;
+    private ClienteRepository clienteRepository;
 
     @GetMapping("/clientes")
     public List<Cliente> listar(){
-
-        return manager.createQuery("from Cliente", Cliente.class)
-                .getResultList();
+        return clienteRepository.findAll();
     }
+
+    @GetMapping("/clientes/{clienteId}")
+    public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId){
+//        Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+//        if( cliente.isPresent()){
+//            return ResponseEntity.ok(cliente.get());
+//        }else{
+//            return ResponseEntity.notFound().build();
+//        }
+
+        return clienteRepository.findById(clienteId)
+                .map(cliente ->ResponseEntity.ok(cliente) )
+                .orElse(ResponseEntity.notFound().build());
+
+    }
+
 }
